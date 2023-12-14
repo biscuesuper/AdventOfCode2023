5f5ba4f144ff7b98c6f6e14243fd19a129456cd5
biscuesuper <ioana.bacrau@gmail.com>
2023-12-14 17:16:43 +0000

    template proj

---
+++ template/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        Read();
+    }
+
+    static List<string> lines = new List<string>();
+
+    public static void Read()
+    {
+        try
+        {
+            StreamReader reader = new StreamReader("../../../in.txt");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                lines.Add(line);
+            }
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+}
